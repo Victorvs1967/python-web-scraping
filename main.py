dda8d7d5977cc54af3c58472c3bec0b93fa2ba54
Victorvs1967 <victorsmirnov@me.com>
2023-03-27 17:30:07 +0300

    probe features

--- main.py
+++ main.py
@@ -5,38 +5,37 @@ from bs4 import BeautifulSoup
 
 
 # config constants
-startUrl = 'https://scrapeme.live/shop/page/1/' # start link
-data = [] # content storage
-visited = set() # list of visited links
-max_visits = 100 # max number links to visit
-num_worker = 5 # number thread to create
-
-# free proxy list - https://free-proxy-list.net
-proxies = {
-	'http': 'http://91.209.11.132:80',
-	'http': 'http://8.219.176.202:8080',
-}
-
-headers = {
-	'authority': 'httpbin.org',
-	'cache-control': 'max-age=0',
-	'sec-ch-ua': '"Chromium";v="92", " Not A;Brand";v="99", "Google Chrome";v="92"',
-	'sec-ch-ua-mobile': '?0',
-	'upgrade-insecure-requests': '1',
-	'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36',
-	'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-	'sec-fetch-site': 'none',
-	'sec-fetch-mode': 'navigate',
-	'sec-fetch-user': '?1',
-	'sec-fetch-dest': 'document',
-	'accept-language': 'en-US,en;q=0.9',
+config = {
+  'startUrl': 'https://scrapeme.live/shop/page/1/', # start link
+  'data': [], # content storage
+  'visited': set(), # list of visited links
+  'max_visits': 100, # max number links to visit
+  'num_worker': 5, # number thread to create
+  'proxies': {  # free proxy list - https://free-proxy-list.net
+    'http': 'http://91.209.11.132:80',
+    'http': 'http://8.219.176.202:8080',
+  },
+  'headers': {
+    'authority': 'httpbin.org',
+    'cache-control': 'max-age=0',
+    'sec-ch-ua': '"Chromium";v="92", " Not A;Brand";v="99", "Google Chrome";v="92"',
+    'sec-ch-ua-mobile': '?0',
+    'upgrade-insecure-requests': '1',
+    'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36',
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'sec-fetch-site': 'none',
+    'sec-fetch-mode': 'navigate',
+    'sec-fetch-user': '?1',
+    'sec-fetch-dest': 'document',
+    'accept-language': 'en-US,en;q=0.9',
+  },
 }
 
 # get html content
 def get_html(url):
 	try:
 		# response = requests.get(url)
-		response = requests.get(url, headers=headers, proxies=proxies)
+		response = requests.get(url, headers=config['headers'], proxies=config['proxies'])
 		return response.content
 	except Exception as e:
 		print(e)
@@ -44,12 +43,12 @@ def get_html(url):
 
 # get list of links
 def extract_links(soup):
-  return [a.get('href') for a in soup.select('a.page-numbers') if a.get('href') not in visited]
+  return [a.get('href') for a in soup.select('a.page-numbers') if a.get('href') not in config['visited']]
 
 # get need content
 def extract_content(soup):
   for product in soup.select('.product'):
-    data.append({
+    config['data'].append({
       'id': product.find('a', attrs={'data-product_id': True})['data-product_id'],
       'name': product.find('h2').text,
       'price': product.find('span', {'class': 'amount'}).text
@@ -57,19 +56,19 @@ def extract_content(soup):
 
 # main crawler function
 def crawl(url, q):
-  visited.add(url)
+  config['visited'].add(url)
   print('Crawl: ', url)
   html = get_html(url)
   soup = BeautifulSoup(html, 'lxml')
   extract_content(soup)
   links = extract_links(soup)
-  [q.put(link) for link in links if link not in visited]
+  [q.put(link) for link in links if link not in config['visited']]
 
 # create crawl queue worker
 def queue_worker(i, q):
   while True:
     url = q.get()  # get an item from the queue
-    if (len(visited) < max_visits and url not in visited):
+    if (len(config['visited']) < config['max_visits'] and url not in config['visited']):
       crawl(url, q)
     q.task_done()  # notifies the queue that the item has been processed
 
@@ -77,23 +76,22 @@ def queue_worker(i, q):
 def main():
   q = queue.Queue()
   # oganize threeding queue
-  for i in range(num_worker):
+  for i in range(config['num_worker']):
     Thread(target=queue_worker, args=(i, q), daemon=True).start()
-  q.put(startUrl)
+  q.put(config['startUrl'])
   q.join()
 
   # output result
   print('\nVisited: ')
-  [print(visit) for visit in visited]
+  [print(visit) for visit in config['visited']]
   print('\nProducts:')
-  [print(product) for product in data]
+  [print(product) for product in config['data']]
   print('\nDone')
 
-
-def ip_rate_limit(url):
-   response = requests.get(url)
-   return response.json()['origin']
+def get_origin_ip():
+   return requests.get('http://httpbin.org/ip', proxies=config['proxies'], headers=config['headers']).json()['origin']
 
 if __name__ == '__main__':
-  main()
-
+  # main()
+  
+  print(get_origin_ip())
